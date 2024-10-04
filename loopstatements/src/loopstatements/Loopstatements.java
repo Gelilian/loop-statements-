1a335b87298c3007ebf90ead2af06c7fb9329062
Gelilian <2300715487@mubs.ac.ug>
2024-10-04 16:46:17 -0700

    first commit

---
+++ loopstatements/src/loopstatements/Loopstatements.java
@@ -0,0 +1,13 @@
+package loopstatements;
+
+public class Loopstatements {
+
+	public static void main(String[] args) {
+		// Loop to print even numbers from 10 to 20
+		for(int z = 10; z <=20; z += 2) {
+			System.out.println(z);
+		}
+
+	}
+
+}
